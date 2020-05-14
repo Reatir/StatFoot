c9d1cb7186203c0ec5b0c44f11e6d57bc89aee22
Reatir <bastien.fernandes.3g3@gmail.com>
2020-05-14 14:32:32 +0200

    ajout fonction ajout equipe

--- CDivision.py
+++ CDivision.py
@@ -1,7 +1,13 @@
+
+
 class CDivision(object):
 
 
     match =[]
+    equipe = []
 
     def add_match(self,match_):
         self.match.append(match_)
+
+    def add_equipe(self,equipe):
+        self.match.append(equipe)
