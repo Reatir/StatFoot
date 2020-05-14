2c063eb8eb4c94a352963c3f09867e063cf9c801
Reatir <bastien.fernandes.3g3@gmail.com>
2020-05-14 11:58:50 +0200

    ajout des classe de division et saison

---
+++ CDivision.py
@@ -0,0 +1,7 @@
+class CDivision(object):
+
+
+    match =[]
+
+    def add_match(self,match_):
+        self.match.append(match_)
---
+++ CSaison.py
@@ -0,0 +1,8 @@
+class CSaison(object):
+
+    division = []
+
+
+
+    def add_division(self, division_):
+        self.division.append(division_)
