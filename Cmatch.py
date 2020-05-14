825524d6d96a9ff19ba99d83e4aca5dfd6b1e906
Reatir <bastien.fernandes.3g3@gmail.com>
2020-05-14 12:29:17 +0200

    ajout recuperaztion et archivage des stats voulu

--- Cmatch.py
+++ Cmatch.py
@@ -1,9 +1,15 @@
-class Cmatch :
+class Cmatch(object):
 
     ScoreHomeTeam =0
     ScoreAwayTeam =0
+    NameHomeTeam =""
+    NameAwayTeam =""
 
-    def __init__(self,ScoreHomeTeam_,ScoreAwayTeam_):
-       ScoreHomeTeam = ScoreHomeTeam_
-       ScoreAwayTeam = ScoreAwayTeam_
+
+    def __init__(self,ScoreHomeTeam_,ScoreAwayTeam_,HomeTeam_,AwayTeam_):
+       self.ScoreHomeTeam = ScoreHomeTeam_
+       self.ScoreAwayTeam = ScoreAwayTeam_
+       self.NameHomeTeam = HomeTeam_
+       self.NameAwayTeam = AwayTeam_
+
 
--- ReadCSV.py
+++ ReadCSV.py
@@ -1,14 +1,29 @@
 import csv
+import StatFoot.CSaison
+import StatFoot.CDivision
+import StatFoot.Cmatch
 
+saison = StatFoot.CSaison.CSaison()
 
 def readCSV():
 
- with open('F1.csv') as csv_file:
-     csv_reader = csv.reader(csv_file,delimiter=',')
-     line_count = 0
-     for row in csv_reader:
-         if line_count == 0:
-             print(f'Column names are {", ".join(row)}')
-             line_count += 1
+        division = StatFoot.CDivision.CDivision()
+        with open('F1.csv') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+
+                #if line_count == 0:
+                 #   print(f'Column names are {", ".join(row)}')
+
+                if line_count>1:
+                    match = StatFoot.Cmatch.Cmatch(row["FTHG"],row["FTAG"],row["HomeTeam"],row["AwayTeam"])
+                    division.add_match(match)
+                line_count += 1
+
+        saison.add_division(division)
+
+
 
 readCSV()
+
