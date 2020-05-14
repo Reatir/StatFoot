514d37248c6809ea3217b1dc22aae8ecc8cd4870
Reatir <bastien.fernandes.3g3@gmail.com>
2020-05-14 11:03:59 +0200

    initial commit

---
+++ Cmatch.py
@@ -0,0 +1,9 @@
+class Cmatch :
+
+    ScoreHomeTeam =0
+    ScoreAwayTeam =0
+
+    def __init__(self,ScoreHomeTeam_,ScoreAwayTeam_):
+       ScoreHomeTeam = ScoreHomeTeam_
+       ScoreAwayTeam = ScoreAwayTeam_
+
